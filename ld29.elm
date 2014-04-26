2f5c4cc855c526708705105b910160bf9dd1347c
bucketh3ad <scrippman@gmail.com>
2014-04-26 14:42:11 -0600

    Add basic graphics; rotate player based on keyboard input

--- ld29.elm
+++ ld29.elm
@@ -27,17 +27,47 @@ input = sampleOn delta (Input <~ Keyboard.space
                                ~ delta)
 
 --UPDATE SECTION
+stepPlayer : Input -> Player -> Player
+stepPlayer i p = {p | angle <- p.angle - ( toFloat i.dx * i.dt * 2) }
+
 stepGame : Input -> Game -> Game
-stepGame i g = defaultGame
+stepGame ({space,dx,dy,dt} as i) ({state,player} as g) =
+  {g | player <- stepPlayer i player}
 
 
 gameState : Signal Game
 gameState = foldp stepGame defaultGame input
+
 --DRAWING SECTION
 
-drawGame : Game -> Element
-drawGame input = asText input
+background : Form
+background = group
+  [ rect 800 600 |> filled black
+  , rect 50 600 |> filled white
+    |> move (-375,0)
+  , rect 50 600 |> filled white
+    |> move (375, 0)
+  , rect 800 50 |> filled white
+    |> move (0,-275)
+  , rect 800 50 |> filled white
+    |> move (0, 275)
+  ]
 
+drawPlayer : Form
+drawPlayer = polygon [(0,15),(-10,-10),(10,-10)] |> outlined (solid white)
+
+drawGame : Game -> Element
+drawGame ({state,player} as game) =
+  collage 800 600
+    [ background
+    , drawPlayer
+      |> move (player.x, player.y)
+      |> rotate player.angle
+    , toForm (asText "A test" |> color white)
+      |> move (0, 100)
+    , toForm (asText player |> color white)
+      |> move (0, -100)
+    ]
 
 --MAIN GAME LOOP
 main : Signal Element
